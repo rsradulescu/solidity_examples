58c140011841fa57cf98e64a5f992fa55a180656
Rocio Radu <rsradulescu@gmail.com>
2024-10-29 18:42:34 +0100

    Update MyContract.sol
    add mapping example

--- src/MyContract.sol
+++ src/MyContract.sol
@@ -10,10 +10,27 @@ contract MyContract {
     string public myString = "Hello world!";
     bytes32 public myByte32 = "Hello bytes32";
 
+    // arrays 
+    uint[] public uintArray  = [1,2,3];
+    string[] public stringArray = ["apple", "banana"];
+    string[] public values;
+
+    function addValue(string memory _value) public{
+        values.push(_value);
+    }
+
+    function countValues() public view returns (uint){
+        return values.length;
+    }
+
+
+    //mapping vars
+    mapping(uint => string ) names;
+
+
     // adress in the blockchain
     address public myAdress = 0x5B38Da6a701c568545dCfcB03FcB875f56beddC4;
 
-    //struct type
     struct MyStruct {
         uint256 myUnit;
         string myString;
