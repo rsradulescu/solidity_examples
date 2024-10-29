81e4b3241104fe32001b96ee1e43e5656c902caf
Rocio Radu <rsradulescu@gmail.com>
2024-10-29 19:22:37 +0100

    Create ConditionalLoop.sol

--- src/ConditionalLoop.sol
+++ src/ConditionalLoop.sol
@@ -1,1 +1,35 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity >=0.7.0 <0.9.0;
+
+contract ConditionalLoop {
+    
+    uint[] public numbers = [1,2,3,4,5];
+    address public owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    //Loop
+    function countEvenNumbers() public view returns(uint){
+        uint count = 0;
+
+        for (uint i = 0; i < numbers.length; i++) 
+        {
+            if(isEvenNumber(numbers[i])){
+                count++;
+            }
+        }
+        return count;
+    }
+    //Conditions if-then-else
+    function isEvenNumber(uint _number) public pure returns(bool){
+        return (_number % 2 == 0);
+    }
+
+    function isOwner() public view returns(bool){
+        return (msg.sender == owner) ;
+    }
+
+}
