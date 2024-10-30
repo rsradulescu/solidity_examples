370dc3d7ab38abc8f27092854c9f1a39b457567f
Rocio Radu <rsradulescu@gmail.com>
2024-10-30 12:29:55 +0100

    Update Inheritance.sol

--- src/Inheritance.sol
+++ src/Inheritance.sol
@@ -1,1 +1,47 @@
+// SPDX-License-Identifier: GPL-3.0
 
+pragma solidity ^0.8.0;
+
+// owner contract
+contract Ownable {
+    address owner;
+
+    modifier onlyOwner(){
+        require(msg.sender == owner, "Must be owner.");
+        _;
+    }
+
+    constructor()  {
+        owner = msg.sender;
+    }
+
+}
+// secret contract
+contract SecretVault{
+    string secret;
+    
+    constructor(string memory _secret)  {
+        secret = _secret;
+    }
+    
+    function getSecret() public view returns(string memory){
+        return secret;
+    } 
+}
+
+// Factory of smart contract 
+contract Inheritance is Ownable{
+
+    address secretVault;
+    
+    constructor(string memory _secret){
+        SecretVault _secretVault = new SecretVault(_secret);
+        secretVault = address(_secretVault);
+        super; // call to Ownable constructor
+    }
+
+    function getSecret() public view onlyOwner returns(string memory){
+        return SecretVault(secretVault).getSecret();
+    } 
+
+}
